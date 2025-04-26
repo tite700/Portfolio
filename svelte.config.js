e1146c2060c4cb386400ac6a29ddde9153f8c787
Shaamallow <eyal.benaroche@gmail.com>
2025-04-26 14:45:22 +0200

    Init commit

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), mdsvex()],
+
+  kit: {
+    adapter: adapter(),
+    alias: {
+      '@/*': './src/lib/*'
+    }
+  },
+
+  extensions: ['.svelte', '.svx']
+};
+
+export default config;
