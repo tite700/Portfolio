be9bd540a2cfb9027ad0734bf6cf64235abf88c9
tite700 <tite7001@gmail.com>
2025-05-01 12:51:43 +0200

    feat: ca marche chez moi

---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="align flex w-full items-center justify-center px-5">
+	<h1 class="scroll-m-20 pt-[10rem] text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+		Mes posts
+	</h1>
+</div>
---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="align flex w-full items-center justify-center px-5">
+	<h1 class="scroll-m-20 pt-[10rem] text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+		Resume
+	</h1>
+</div>
