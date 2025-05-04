8a5c017fd317ffe0ae1bafdacd78bf377b76cd29
tite700 <tite7001@gmail.com>
2025-05-04 11:51:52 +0200

    feat: update scene with lights and shadows

--- src/lib/components/scene.svelte
+++ src/lib/components/scene.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
-	import { OrbitControls, Gizmo, type GizmoOptions } from '@threlte/extras';
+	import { OrbitControls, Gizmo } from '@threlte/extras';
+	import * as THREE from 'three';
 </script>
 
-<T.PerspectiveCamera makeDefault position={[5, 5, 5]}>
+<T.PerspectiveCamera makeDefault position={[5, 8, 10]}>
 	<OrbitControls />
 	<Gizmo />
 </T.PerspectiveCamera>
-<T.Mesh position={[0, 0, 0]}>
+
+<T.DirectionalLight position={[10, 15, 10]} intensity={1.2} castShadow />
+
+<T.AmbientLight intensity={0.4} />
+
+<T.Mesh position={[0, 1, 0]} castShadow>
 	<T.BoxGeometry args={[1, 1, 1]} />
-	<T.MeshBasicMaterial color="red" />
+	<T.MeshStandardMaterial color="tomato" roughness={0.4} metalness={0.3} />
 </T.Mesh>
 
-<T.Mesh position={[0, 0, -2]} rotation={[Math.PI, 0, 0]}>
-	<T.PlaneGeometry args={[10, 10]} />
-	<T.MeshBasicMaterial color="grey" />
+<T.Mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+	<T.PlaneGeometry args={[30, 30]} />
+	<T.MeshStandardMaterial color="lightgrey" roughness={1} metalness={0} side={THREE.DoubleSide} />
 </T.Mesh>
